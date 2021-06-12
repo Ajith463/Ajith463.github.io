e62868dffd10b958a6052f225b7edcee242af34e
Ajith463 <92643@fhss.sjp.ac.lk>
2021-06-12 05:54:19 +0530

    Updates

---
+++ precache-manifest.64a9f95184e498ba29eab41f3910202e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa2fd9e238d7ce56a5cd7b3c85f07f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1b6806e36792f9a599f",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b6806e36792f9a599f",
+    "url": "/static/js/main.2b732243.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "97c09b1a568813f9f0a57d945ad07495",
+    "url": "/static/media/mypic.97c09b1a.jpg"
+  },
+  {
+    "revision": "12def68e77a90cf1e435e4ba7781afab",
+    "url": "/static/media/resume.12def68e.pdf"
+  }
+]);
